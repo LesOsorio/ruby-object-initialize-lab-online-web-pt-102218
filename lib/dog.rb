89f87c49743d377f5e4c14f546d314aaf94f5bd4
Leslie Osorio <lesosorio17@gmail.com>
2018-11-17 21:34:26 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,22 +3,23 @@
     def initialize(name)
       @name = name
     end
-    
+
     def name=(name)
       @name = name
     end
-    
+
     def name
       name
     end
-    
+
     def breed(breed)
       @breed = breed
-    end 
-    
+    end
+
     def breed=(breed)
       @breed = breed
     end
+    
     def breed
       breed
     end
